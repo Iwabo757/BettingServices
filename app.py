66c8a5baed661d74cc45fbea37b0bdfeaaf6b8f2
Iwabo757 <nickpallugna@gmail.com>
2026-05-19 11:07:34 -0500

    Fixed PostgreSQL table creation

--- app.py
+++ app.py
@@ -296,8 +296,8 @@ def delete_bet(bet_id):
     return redirect(url_for('history'))
 # START APP
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
